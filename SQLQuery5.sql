0912bcd17d469567986e636b7c30e3a44b8512c3
kize2000 <55257984+kize2000@users.noreply.github.com>
2019-11-12 13:34:43 -0800

    Delete SQLQuery5.sql

--- SQLQuery5.sql
+++
@@ -1,7 +0,0 @@
--- A query that returns vendors without invoices:
-   SELECT VendorID, VendorName, VendorState -- Columns used
-   FROM Vendors -- table used
-   WHERE NOT EXISTS -- condition statement
-         (SELECT *  -- condition used which is all invoices where values from VendorID in Invoices is == VendorID in Vendors
-         FROM Invoices
-         WHERE Invoices.VendorID = Vendors.VendorID);
