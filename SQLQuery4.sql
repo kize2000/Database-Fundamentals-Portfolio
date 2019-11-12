a8278385781c0aba273b0080985b87471f58d619
kize2000 <55257984+kize2000@users.noreply.github.com>
2019-11-12 13:34:34 -0800

    Delete SQLQuery4.sql

--- SQLQuery4.sql
+++
@@ -1,4 +0,0 @@
--- Same query as before just simplified
-SELECT Vendors.VendorID, VendorName, VendorState -- columns used
-FROM Vendors join Invoices -- table used 
-	ON Vendors.VendorID != Invoices.VendorID; -- condition statement as seen in previous query except with does not equal sign (!=)
