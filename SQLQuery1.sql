40d9e0c0d976448650958839760d334889ecb03d
kize2000 <55257984+kize2000@users.noreply.github.com>
2019-11-12 13:33:48 -0800

    Delete SQLQuery1.sql

--- SQLQuery1.sql
+++
@@ -1,7 +0,0 @@
-/*This query creates a brand new database and back ups that database to the c drive*/
-create database Test_1
-backup database Test_1
-To disk = 'C:\Database Backups\Test_1db.bak'
-/*Line 1 created the database "Test_1"
-Line 2 started the backup command 
-and line 3 designated the location in which the backup was made which was the C drive in the Folder "Database Backups"
