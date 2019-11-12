593bd89baad589ac275f4382a4493ed6bc288dce
kize2000 <55257984+kize2000@users.noreply.github.com>
2019-11-12 13:34:03 -0800

    Delete SQLQuery10.sql

--- SQLQuery10.sql
+++
@@ -1,8 +0,0 @@
---The same query restated with a subquery:
-   SELECT invoiceNumber, InvoiceDate, InvoiceTotal -- columns used
-   FROM Invoices -- table used
-   WHERE VendorID IN
-       (SELECT VendorID -- subquery that selects all values from vendors that have VendorState = 'CA'
-        FROM Vendors
-        WHERE VendorState = 'CA')
-   ORDER BY InvoiceDate; -- order results by InvoiceDate by increasing
